262a219c00dec347b94c96d09284310a96b46b05
Adam Bertrand Berger <adambertrandberger@gmail.com>
2019-05-17 21:12:48 -0500

    l is good now

--- src/projects/l/l.js
+++ src/projects/l/l.js
@@ -36,36 +36,6 @@ class L {
         return val;
     }
 
-    add(tag) {
-        this.children.push(tag);
-        return this;
-    }
-
-    id(id) {
-        this.props.id = id;
-        return this;
-    }
-
-    class(cls) {
-        this.props.class = cls;
-        return this;
-    }
-
-    addClass(cls) {
-        this.class += ' ' + cls;
-        return this;
-    }
-
-    set(prop, val) {
-        this.props[prop] = val;
-        return this;
-    }
-
-    style(css) {
-        this.props.style = css;
-        return this;
-    }
-
     render() {
         var tag = document.createElement(this.name);
 
@@ -105,122 +75,18 @@ class L {
 }
 
 const l = (function () {
-    const lib = { tag: (name, init1, init2) => new L(name, init1, init2).render() },
-          tags = [
-              'a',
-              'abbr',
-              'address',
-              'area',
-              'article',
-              'aside',
-              'audio',
-              'b',
-              'base',
-              'bdi',
-              'bdo',
-              'blockquote',
-              'body',
-              'br',
-              'button',
-              'canvas',
-              'caption',
-              'cite',
-              'code',
-              'col',
-              'colgroup',
-              'data',
-              'datalist',
-              'dd',
-              'del',
-              'details',
-              'dfn',
-              'dialog',
-              'div',
-              'dl',
-              'dt',
-              'em',
-              'embed',
-              'fieldset',
-              'figure',
-              'footer',
-              'form',
-              'h1',
-              'h2',
-              'h3',
-              'h4',
-              'h5',
-              'h6',
-              'head',
-              'header',
-              'hgroup',
-              'hr',
-              'html',
-              'i',
-              'iframe',
-              'img',
-              'input',
-              'ins',
-              'kbd',
-              'keygen',
-              'label',
-              'legend',
-              'li',
-              'link',
-              'main',
-              'map',
-              'mark',
-              'menu',
-              'menuitem',
-              'meta',
-              'meter',
-              'nav',
-              'noscript', // :)
-              'object',
-              'ol',
-              'optgroup',
-              'option',
-              'output',
-              'p',
-              'param',
-              'pre',
-              'progress',
-              'q',
-              'rb',
-              'rp',
-              'rt',
-              'rtc',
-              'ruby',
-              's',
-              'samp',
-              'script',
-              'section',
-              'select',
-              'small',
-              'source',
-              'span',
-              'strong',
-              'style',
-              'sub',
-              'summary',
-              'sup',
-              'table',
-              'tbody',
-              'td',
-              'template',
-              'textarea',
-              'tfoot',
-              'th',
-              'thead',
-              'time',
-              'title',
-              'tr',
-              'track',
-              'u',
-              'var',
-              'video',
-              'wbr',
+    const lib = (name, init1, init2) => new L(name, init1, init2).render();
+          tags = [ 
+              'a', 'abbr', 'address', 'area', 'article', 'aside', 'audio', 'b', 'base', 'bdi', 'bdo', 'blockquote', 
+              'body', 'br', 'button', 'canvas', 'caption', 'cite', 'code', 'col', 'colgroup', 'data', 'datalist',
+              'dd', 'del', 'details', 'dfn', 'dialog', 'div', 'dl', 'dt', 'em', 'embed', 'fieldset', 'figure', 'footer',
+              'form', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head', 'header', 'hgroup', 'hr', 'html', 'i', 'iframe', 'img',
+              'input', 'ins', 'kbd', 'keygen', 'label', 'legend', 'li', 'ul', 'link', 'main', 'map', 'mark', 'menu', 'menuitem',
+              'meta', 'meter', 'nav', 'noscript', 'object', 'ol', 'optgroup', 'option', 'output', 'p', 'param', 'pre', 'progress',
+              'q', 'rb', 'rp', 'rt', 'rtc', 'ruby', 's', 'samp', 'script', 'section', 'select', 'small', 'source', 'span',
+              'strong', 'style', 'sub', 'summary', 'sup', 'table', 'tbody', 'td', 'template', 'textarea', 'tfoot', 'th',
+              'thead', 'time', 'title', 'tr', 'track', 'u', 'var', 'video', 'wbr',
           ];
-
     for (const tag of tags) {
         lib[tag] = (init1, init2) => new L(tag, init1, init2).render();
     }
